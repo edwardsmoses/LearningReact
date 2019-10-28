780ef36bfec25efa4d204c129e6953cd129c00d2
edwardsmoses3 <edwardsmoses3@gmail.com>
2019-10-28 18:55:27 +0100

    Initial Commit - Learning React

---
+++ API/Controllers/ReactValuesController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataPersist;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReactValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ReactValuesController(DataPersist.DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Domain.Value>>> Get(){
+            var values = await context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Domain.Value>> Get(int id)
+        {
+            var value = await context.Values.FindAsync(id);
+            return value;
+        }
+    }
+}
---
+++ DataPersist/DataContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DataPersist
+{
+
+    public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<DataContext>();
+          builder.UseSqlite("Data source=learnreact.db");
+        return new DataContext(builder.Options);
+    }
+}
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Value> Values { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Domain.Value>().HasData(
+                new Domain.Value{
+                    Id = 1,
+                    Name = "Value 101"
+                },
+                 new Domain.Value{
+                    Id = 2,
+                    Name = "Value 102"
+                },
+                 new Domain.Value{
+                    Id = 3,
+                    Name = "Value 103"
+                },
+                 new Domain.Value{
+                    Id = 4,
+                    Name = "Value 104"
+                }
+            );
+            
+        }
+
+    }
+}
