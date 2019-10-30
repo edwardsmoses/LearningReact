e63b122b26bc5e27d01c2848524e80f179bd9f89
edwardsmoses3 <edwardsmoses3@gmail.com>
2019-10-30 09:49:34 +0100

    first component with javascript and typescript with react.

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { cars } from './demo';
-import CarItem from './CarItem';
 
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Learning React. <small>30. October 2019.</small>
-        </p>
+class App extends Component{
 
-        <ul>
-          {
-            cars.map((car) => (
-              <CarItem car={car}></CarItem>
-            ))
-          }
-        </ul>
+  state = {
+    values: []
+  }
+
+  componentDidMount(){
+    this.setState({
+      values: [
+        {id:1, name:'Value 101'},
+        {id:2, name:'Value 102'}
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         
+         <img src={logo} className="App-logo" alt='Logo'></img>
+
+         <ul>
+           {
+             this.state.values.map((value:any) => (
+               <li>{value.id} - {value.name}</li>
+             ))
+           }
+         </ul>
+        
+        </header>
+      </div>
       
-      </header>
-    </div>
-    
-  );
+    );
+  }
 }
+
 
 export default App;
